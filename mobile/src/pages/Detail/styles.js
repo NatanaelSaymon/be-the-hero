a399894a515211829126abd5a27514a39bad4aa3
Natanael Saymon <nsaymonsouza@gmail.com>
2020-03-26 23:21:39 -0300

    Mobile da aplicação

---
+++ mobile/src/pages/Detail/styles.js
@@ -0,0 +1,77 @@
+import { StyleSheet } from 'react-native';
+import Constants from 'expo-constants';
+export default StyleSheet.create({
+    container: {
+        flex: 1,
+        paddingHorizontal: 24,
+        paddingTop: Constants.statusBarHeight + 20,
+    },
+
+    header: {
+        flexDirection: 'row',
+        justifyContent: 'space-between',
+        alignItems: 'center',
+    },
+
+    incident: {
+        marginBottom: 16,
+        marginTop: 48,
+        padding: 24,
+        borderRadius: 8,
+        backgroundColor: '#fff',
+    },
+
+    incidentProperty: {
+        fontSize: 14,
+        color: '#41414d',
+        fontWeight: 'bold',
+        marginTop: 24,
+    },
+
+    incidentValue: {
+        marginTop: 8,
+        fontSize: 15,
+        color: '#737380',
+    },
+
+    contatcBox:{
+        marginBottom: 16,
+        padding: 24,
+        borderRadius: 8,
+        backgroundColor: '#fff',
+    },
+
+    heroTitle: {
+        fontWeight: 'bold',
+        fontSize: 20,
+        color: '#13131a',
+        lineHeight: 30
+    },
+
+    heroDescription: {
+        fontSize: 15,
+        color: '#737380',
+        marginTop: 16
+    },
+
+    actions:{
+        marginTop: 16,
+        flexDirection: 'row',
+        justifyContent: 'space-between'
+    },
+
+    action:{
+        backgroundColor: '#e02041',
+        borderRadius: 8,
+        height: 50,
+        width: '48%',
+        justifyContent: 'center',
+        alignItems: 'center'
+    },
+
+    actionText:{
+        color: '#fff',
+        fontSize: 15,
+        fontWeight: 'bold'
+    }
+})
